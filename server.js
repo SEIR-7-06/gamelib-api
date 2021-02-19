e6b20760b07d7aece058ced97d95201f18c9bdba
kbbushman <k.bushman@yahoo.com>
2021-02-19 09:58:00 -0800

    Initial commit, adds server boilerplate

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000
+
+// Home Route
+app.get('/', (req, res) => {
+  res.send('<h1>GameLib API</h1>')
+});
+
+
+// Listen For Requests
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
