d42e176982d2676dc55bdc0061c9236516285442
kbbushman <k.bushman@yahoo.com>
2021-02-19 10:20:51 -0800

    Adds Game model

--- models/index.js
+++ models/index.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 
 const connectionString = 'mongodb://localhost:27017/gamelib-1207';
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
 
-mongoose.connect(connectionString)
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+})
   .then(() => console.log('MongoDB connected successfully'))
   .catch((err) => console.log(err));
+
+
+module.exports = {
+  Game: require('./Game'),
+};
