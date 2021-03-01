ac240c121644bc261088937be7d0bf2f57985b39
kbbushman <k.bushman@yahoo.com>
2021-03-01 09:19:52 -0800

    Adds heroku config

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb://localhost:27017/gamelib-1207';
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/gamelib-1207';
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
